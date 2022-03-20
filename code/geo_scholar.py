8070b4d1eed536f5894f4aba3344d639c30694d7
jiaxin.du <fashui01@163.com>
2022-03-19 22:29:30 -0500

    init map

--- code/geo_scholar.py
+++ code/geo_scholar.py
@@ -1,34 +1,44 @@
 #from glob import glob
+import pdb
+import json
 import pandas as pd
 import spacy
 from tqdm import tqdm
-#all_files = glob("cleaned/*/*.txt")
-df=pd.read_csv('../../../Documents/uri/UriTweetLocationImage.csv', lineterminator='\n')
-text_list = df["text"].to_list()
-nlp = spacy.load("en_core_web_trf", disable=["tagger", "parser", "attribute_ruler", "lemmatizer"])
-for text in text_list:
-    doc = nlp(text)
-    i=0
-    for entity in doc.ents:
-        if entity.label_ == "GPE":
-            print(text+" GPE "+entity.text)
-        elif entity.label_ == "LOC":
-            print(text+" LOC "+entity.text)
-        elif entity.label_ == "FAC":
-            print(text+" FAC "+entity.text)
-
-
 from mordecai import Geoparser
 geo = Geoparser()
-
+df = pd.read_excel("Pub_abstract.xlsx")
+df = df.dropna(subset=['abstract'])
 parsed_list = []
-for text in tqdm(texts):
+titles = []
+abstracts = []
+paperIds = []
+place_names = []
+lons = []
+lats = []
+words = []
+countrys = []
+for i in tqdm(range(0,len(df))):
     try:
-        doc = geo.geoparse(text)
-        parsed_list.append(doc)
+        abs = df['abstract'].iloc[i]
+        doc = geo.geoparse(abs)
+        for single_place in doc:
+            lons.append(single_place["geo"]['lon'])
+            lats.append(single_place["geo"]['lat'])
+            words.append(single_place["word"])
+            countrys.append(single_place["country_code3"])
+            abstracts.append(abs)
+            titles.append(df['publication_api'].iloc[i])
+            paperIds.append(df["publication_uri"].iloc[i])
     except:
-        print(text)
-        parsed_list.append("error")
+        print(abs)
+        continue
+
+pdb.set_trace()
+df2 =  pd.DataFrame(data={"place_name": words,
+                    "lon": lons,
+"lat": lats, "word": words, "title": titles, "country": countrys, "paperId": paperIds})
+df2.to_json('encoded_scholar.json', orient= 'records')
 
 
-res = [json.loads(idx.replace("'", '"')) for idx in parsed_list]
+
+
